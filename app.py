39295baced5c9b867f0f124e871c9e34797f4174
Eric <eric.farias000@gmail.com>
2026-04-16 13:47:17 -0300

    Primeira versão do DataPeek

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("DataPeek - Seu CSV Explicado")
+st.write("Faça upload do seu arquivo CSV para obter insights e visualizações rápidas!   ")
+
+arquivo = st.file_uploader("Escolha um arquivo CSV", type="csv")
+
+if arquivo is not None:
+    df = pd.read_csv(arquivo)
+    st.success("Arquivo carregado com sucesso!")
+
+    st.subheader("Valores nulos por coluna")
+    st.write(df.isnull().sum())
+
+    st.subheader("Estatísticas das colunas numéricas")
+    st.write(df.describe())
+
+else:
+    st.info("Aguardando o upload do arquivo CSV...")
+
+st.subheader("Gráficos")
+
+colunas_numericas = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+
+if len(colunas_numericas) > 0:
+    coluna_escolhida = st.selectbox("Escolha uma coluna numérica: ", colunas_numericas)
+
+    fig, ax = plt.subplots()
+    ax.hist(df[coluna_escolhida].dropna(), bins=20, color='skyblue', edgecolor='black')
+    ax.set_title(f"Distribuição de {coluna_escolhida}")
+    st.pyplt(fig)
+else:
+    st.warning("Nenhuma coluna numérica encontrada para gerar gráfico.")
