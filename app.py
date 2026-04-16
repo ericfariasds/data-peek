a5de42687c8937a8ba9fab1c2553c14b0074de63
Eric <eric.farias000@gmail.com>
2026-04-16 14:36:01 -0300

    Correção de erro na leitura dos CSV e dos gráficos

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ st.write("Faça upload do seu arquivo CSV para obter insights e visualizações 
 arquivo = st.file_uploader("Escolha um arquivo CSV", type="csv")
 
 if arquivo is not None:
-    df = pd.read_csv(arquivo)
+    try:
+        df = pd.read_csv(arquivo)
+    except:
+        st.warning("CSV com formatação irregular - algumas linhas foram ignoradas")
+        df = pd.read_csv(arquivo,
+                         on_bad_lines='skip',
+                         encoding='utf-8')
+        
     st.success("Arquivo carregado com sucesso!")
+
+    st.subheader("Colunas do arquivo")
+    st.write(list(df.columns))
 
     st.subheader("Valores nulos por coluna")
     st.write(df.isnull().sum())
@@ -17,19 +27,21 @@ if arquivo is not None:
     st.subheader("Estatísticas das colunas numéricas")
     st.write(df.describe())
 
+    st.subheader("Gráficos")
+    colunas_numericas = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+
+
+    if len(colunas_numericas) > 0:
+        coluna_escolhida = st.selectbox("Escolha uma coluna numérica: ", colunas_numericas)
+
+        fig, ax = plt.subplots()
+        ax.hist(df[coluna_escolhida].dropna(), bins=20, color='skyblue', edgecolor='black')
+        ax.set_title(f"Distribuição de {coluna_escolhida}")
+        st.pyplot(fig)
+    else:
+        st.warning("Nenhuma coluna numérica encontrada para gerar gráfico.")
+
 else:
     st.info("Aguardando o upload do arquivo CSV...")
 
-st.subheader("Gráficos")
 
-colunas_numericas = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
-
-if len(colunas_numericas) > 0:
-    coluna_escolhida = st.selectbox("Escolha uma coluna numérica: ", colunas_numericas)
-
-    fig, ax = plt.subplots()
-    ax.hist(df[coluna_escolhida].dropna(), bins=20, color='skyblue', edgecolor='black')
-    ax.set_title(f"Distribuição de {coluna_escolhida}")
-    st.pyplt(fig)
-else:
-    st.warning("Nenhuma coluna numérica encontrada para gerar gráfico.")
